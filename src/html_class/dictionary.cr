610d7f75768aa4f8bf924ba52c4dcaf232e61432
Ian White <ian.w.white@gmail.com>
2023-05-16 17:34:39 +0100

    Remove unecessary type aliases

--- src/html_class/dictionary.cr
+++ src/html_class/dictionary.cr
@@ -3,16 +3,13 @@ module HTMLClass
   # Contains a reference to a merge strategy, which is used when a key already exists, and
   # the #add collision strategy is set to :merge.
   class Dictionary
-    alias K = Symbol | Set(Symbol)
-    alias V = String
-
-    @hash : Hash(K, V)
+    @hash : Hash(Symbol | Set(Symbol), String)
 
     protected getter merge : HTMLClassMerge::Merge
 
     delegate :[], :[]?, to: @hash
 
-    def initialize(@hash = Hash(K, V).new, @merge = HTMLClass.default_merge)
+    def initialize(@hash = Hash(Symbol | Set(Symbol), String).new, @merge = HTMLClass.default_merge)
     end
 
     def initialize(other : Dictionary)
@@ -24,21 +21,25 @@ module HTMLClass
       @hash.dup
     end
 
-    def add(key : K, html_class : V, on_collision : OnCollision = :merge) : self
+    def dup
+      self.class.new(self)
+    end
+
+    def add(key : Symbol | Set(Symbol), html_class : String, on_collision : OnCollision = :merge) : self
       self.class.new(@hash.merge({key => handle_collision(key, html_class, on_collision)}), @merge)
     end
 
-    def add(key : Array(Symbol), html_class : V, on_collision : OnCollision = :merge) : self
+    def add(key : Array(Symbol), html_class : String, on_collision : OnCollision = :merge) : self
       add key.to_set, html_class, on_collision
     end
 
     def add(dictionary : Dictionary, on_collision : OnCollision = :merge) : self
-      dictionary.to_h.reduce(self.class.new(self)) do |result, (key, html_class)|
+      dictionary.to_h.reduce(self.dup) do |result, (key, html_class)|
         result.add key, html_class, on_collision
       end
     end
 
-    private def handle_collision(key, html_class, on_collision) : V
+    private def handle_collision(key, html_class, on_collision)
       if on_collision == OnCollision::Merge && (prev_html_class = @hash[key]?)
         @merge.merge [prev_html_class, html_class]
       else
